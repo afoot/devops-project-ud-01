c361afa99f0729855494564cd95fd7b822510be7
wandering-mono <locobu@icloud.com>
2023-03-03 15:42:14 +0400

    111. EC2 Instances.

--- Section 10 - AWS Part 1/resources/morInEec2_part1.sh
+++ Section 10 - AWS Part 1/resources/morInEec2_part1.sh
@@ -4,3 +4,12 @@ sudo apt update && \
   unzip 2128_tween_agency.zip && \
   sudo cp -r 2128_tween_agency/* /var/www/html/ && \
   sudo systemctl restart apache2
+
+
+sudo yum update && \
+  sudo yum install httpd wget unzip -y && \
+  wget https://www.tooplate.com/zip-templates/2128_tween_agency.zip && \
+  unzip 2128_tween_agency.zip && \
+  sudo cp -r 2128_tween_agency/* /var/www/html/ && \
+  sudo systemctl start httpd && \
+  sudo systemctl enable httpd
