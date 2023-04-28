79d976144b2e841bb65006951a0f70aacc8dd6b2
wandering-mono <code@wandering-mono.top>
2023-04-28 13:23:56 +0400

    238. Deployment

--- Section 21 - Kubernetes/kops-commands.sh
+++ Section 21 - Kubernetes/kops-commands.sh
@@ -9,6 +9,17 @@ kops create cluster \
   --node-volume-size=8 \
   --master-volume-size=8
 
+kops create cluster \
+  --name=kubevpro.wandering-mono.top \
+  --state=s3://vprofile-kops-state-mono \
+  --zones=us-east-1a,us-east-1b \
+  --node-count=2 \
+  --node-size=t2.micro \
+  --master-size=t3a.small \
+  --dns-zone=kubevpro.wandering-mono.top \
+  --node-volume-size=8 \
+  --master-volume-size=8
+
 kops update cluster \
   --name kubevpro.wandering-mono.top \
   --state=s3://vprofile-kops-state-mono \
@@ -18,14 +29,8 @@ kops update cluster \
 kops validate cluster \
   --name kubevpro.wandering-mono.top \
   --state=s3://vprofile-kops-state-mono \
-  --wait 3m
+  --wait 10m
   
-kops update cluster \
-  --name kubevpro.wandering-mono.top \
-  --state=s3://vprofile-kops-state-mono \
-  --node-count=0 \
-  --yes
-
 kops delete cluster \
   --name=kubevpro.wandering-mono.top \
   --state=s3://vprofile-kops-state-mono \
