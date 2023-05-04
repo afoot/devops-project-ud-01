67f7b1192bafb4308d21feabaa2ca9fa1439bb14
wandering-mono <code@wandering-mono.top>
2023-05-04 21:45:08 +0400

    Section 22 - App Deployment in Kubernetes Ckuster completed. 262. URL for Website & Wrap up

---
+++ Section 22 - App Deployment on Kubernetes Cluster/kops-commands.sh
@@ -0,0 +1,37 @@
+kops create cluster \
+  --name=kubevpro.wandering-mono.top \
+  --state=s3://vprofile-kops-state-mono \
+  --zones=us-east-1a,us-east-1b \
+  --node-count=2 \
+  --node-size=t3a.small \
+  --master-size=t3a.medium \
+  --dns-zone=kubevpro.wandering-mono.top \
+  --node-volume-size=8 \
+  --master-volume-size=8
+
+kops create cluster \
+  --name=kubevpro.wandering-mono.top \
+  --state=s3://vprofile-kops-state-mono \
+  --zones=us-east-1a,us-east-1b \
+  --node-count=2 \
+  --node-size=t2.micro \
+  --master-size=t3a.small \
+  --dns-zone=kubevpro.wandering-mono.top \
+  --node-volume-size=8 \
+  --master-volume-size=8
+
+kops update cluster \
+  --name kubevpro.wandering-mono.top \
+  --state=s3://vprofile-kops-state-mono \
+  --yes \
+  --admin
+
+kops validate cluster \
+  --name kubevpro.wandering-mono.top \
+  --state=s3://vprofile-kops-state-mono \
+  --wait 10m
+  
+kops delete cluster \
+  --name=kubevpro.wandering-mono.top \
+  --state=s3://vprofile-kops-state-mono \
+  --yes
